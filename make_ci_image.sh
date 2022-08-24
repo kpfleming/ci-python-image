3d2b7ef5ab547e103818de0aa9e5a312a60b8b9c
Kevin P. Fleming <kevin@km6g.us>
2022-08-23 20:35:00 -0400

    Initial commit, import from pvs-hass-mqtt

---
+++ make_ci_image.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -ex
+
+scriptdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+
+py_deps=(build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev)
+
+pyversions=(3.8.13 3.9.13 3.10.6 3.11.0rc1)
+
+c=$(buildah from debian:bullseye)
+
+buildcmd() {
+    buildah run --network host "${c}" -- "$@"
+}
+
+buildah config --workingdir /root "${c}"
+
+buildcmd apt update --quiet=2
+buildcmd apt install --yes --quiet=2 git
+
+buildcmd apt install --yes --quiet=2 "${py_deps[@]}"
+
+for pyver in "${pyversions[@]}"; do
+    # shellcheck disable=SC2001
+    # strip off any beta or rc suffix to get version directory
+    verdir=$(echo "${pyver}" | sed -e 's/^\([[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\).*$/\1/')
+    wget --quiet --output-document - "https://www.python.org/ftp/python/${verdir}/Python-${pyver}.tgz" | tar --extract --gzip
+    buildah run --network host --volume "${scriptdir}:/scriptdir" --volume "$(pwd)/Python-${pyver}:/${pyver}" "${c}" -- /scriptdir/pybuild.sh "/${pyver}"
+    rm -rf "Python-${pyver}"
+done
+
+buildcmd sh -c "rm -rf /usr/local/bin/python3.?m*"
+buildcmd sh -c "rm -rf /usr/local/bin/python3.??m*"
+
+buildcmd pip3.10 install hatch
+buildcmd mkdir /root/hatch
+
+buildcmd pip3.10 install tox
+buildcmd mkdir /root/tox
+
+buildcmd apt remove --yes --purge "${py_deps[@]}"
+buildcmd apt autoremove --yes --purge
+buildcmd apt clean autoclean
+buildcmd sh -c "rm -rf /var/lib/apt/lists/*"
+buildcmd rm -rf /root/.cache
+
+# shellcheck disable=SC2154 # image_name set in external environment
+if buildah images --quiet "${image_name}"; then
+    buildah rmi "${image_name}"
+fi
+buildah commit --squash --rm "${c}" "${image_name}"
