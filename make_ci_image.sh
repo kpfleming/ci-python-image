9b8b9c5c558011d6a6f8774294ad1b89a516e8e8
Kevin P. Fleming <kevin@km6g.us>
2022-09-07 10:24:31 -0400

    Update to include latest versions

--- make_ci_image.sh
+++ make_ci_image.sh
@@ -7,7 +7,7 @@ distro="${1}"
 
 py_deps=(build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev)
 
-pyversions=(3.8.13 3.9.13 3.10.6 3.11.0rc1)
+pyversions=(3.8.14 3.9.14 3.10.7 3.11.0rc1)
 
 c=$(buildah from "debian:${distro}")
 
