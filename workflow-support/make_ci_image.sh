3b8e0d089d69ce5fa366f54f051b91638246908a
Kevin P. Fleming <kevin@km6g.us>
2023-01-24 08:17:57 -0500

    More TOX_CONFIG_FILE workaround.

--- workflow-support/make_ci_image.sh
+++ workflow-support/make_ci_image.sh
@@ -43,8 +43,8 @@ buildah config --env HATCH_CONFIG=/hatch/config.toml "${c}"
 
 buildcmd pip3.11 install tox
 buildcmd mkdir /tox
-buildah copy "${c}" "${scriptdir}/tox-config.ini" /tox/config.ini
-buildah config --env TOX_CONFIG_FILE=/tox/config.ini "${c}"
+# buildah copy "${c}" "${scriptdir}/tox-config.ini" /tox/config.ini
+# buildah config --env TOX_CONFIG_FILE=/tox/config.ini "${c}"
 buildah config --env TOX_WORK_DIR=/tox "${c}"
 
 buildcmd apt-get remove --yes --purge "${py_deps[@]}"
