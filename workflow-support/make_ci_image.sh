17dd2d1df1705203864fa89c0d1b081632e8baef
Kevin P. Fleming <kevin@km6g.us>
2023-12-08 05:54:05 -0500

    Python 3.12.1.

--- workflow-support/make_ci_image.sh
+++ workflow-support/make_ci_image.sh
@@ -11,7 +11,7 @@ py_run_deps=(build-essential libc6-dev libffi-dev)
 
 py_build_deps=(libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libbz2-dev zlib1g-dev)
 
-pyversions=(3.8.18 3.9.18 3.10.13 3.11.6 3.12.0)
+pyversions=(3.8.18 3.9.18 3.10.13 3.11.6 3.12.1)
 
 c=$(buildah from "${base_image}")
 
