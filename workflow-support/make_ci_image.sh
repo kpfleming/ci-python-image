cdb36287d31743c7303d8f93fa703605a1be7d59
Kevin P. Fleming <kevin@km6g.us>
2025-06-21 08:04:38 -0400

    Version 4 of the base image
    * Install Python using uv instead of Hatch
    
    * Add Python 3.14 beta releases
    
    * Use Python 3.13 for running Hatch

--- workflow-support/make_ci_image.sh
+++ workflow-support/make_ci_image.sh
@@ -6,7 +6,7 @@ scriptdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 base_image=${1}; shift
 image_name=${1}; shift
 
-pyversions=(3.9 3.10 3.11 3.12 3.13)
+pyversions=(3.9 3.10 3.11 3.12 3.13 3.14)
 
 c=$(buildah from "${base_image}")
 
@@ -27,27 +27,17 @@ buildah copy "${c}" uv /usr/bin/uv
 buildah copy "${c}" "${scriptdir}/uv-config.toml" /uv.toml
 buildah config --env UV_CONFIG_FILE=/uv.toml "${c}"
 
-buildcmd uv venv --python python3.12 --no-cache /hatch
+buildcmd uv python install "${pyversions[@]}"
 
-buildcmd uv pip install --python /hatch/bin/python3.12 --no-cache hatch hatchling hatch-vcs hatch-fancy-pypi-readme
+buildcmd uv venv --python python3.13 --no-cache /hatch
+
+buildcmd uv pip install --python /hatch/bin/python3.13 --no-cache hatch hatchling hatch-vcs hatch-fancy-pypi-readme
 
 buildah copy "${c}" "${scriptdir}/hatch-config.toml" /hatch/config.toml
 buildah config --env HATCH_CONFIG=/hatch/config.toml "${c}"
 buildah config --env HATCH_ENV_TYPE_VIRTUAL_UV_PATH=/usr/bin/uv "${c}"
 
-# Hatch will add the installed Python distributions as PATH
-# entries in .profile, but that's not necessary since the PATH
-# variable for the entire image will include them
-buildcmd cp /root/.profile /root/.profile.save
-buildcmd /hatch/bin/hatch python install "${pyversions[@]}"
-buildcmd cp /root/.profile.save /root/.profile
-
 new_path=/usr/sbin:/usr/bin:/sbin:/bin:/hatch/bin
-
-for pyversion in "${pyversions[@]}"
-do
-    new_path="${new_path}:/hatch/python/${pyversion}/python/bin"
-done
 
 buildah config --env PATH="${new_path}" "${c}"
 
